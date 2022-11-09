8a946e8ffff13681311adb43a35d6acca9246f07
shige <g2054029@cc.kyoto-su.ac.jp>
2022-11-10 00:05:29 +0900

    veta

---
+++ app/src/main/java/com/example/mesa/models/repository/local/getacountinfo/GetAccountInfo.kt
@@ -0,0 +1,19 @@
+package com.example.mesa.models.repository.local.getacountinfo
+
+import android.Manifest
+import android.app.AppOpsManager
+import android.app.AppOpsManager.OPSTR_GET_USAGE_STATS
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Process
+import androidx.core.content.ContextCompat.getSystemService
+
+
+
+class GetAccountInfo {
+
+
+    // MainActivityのContextを代入
+
+
+}
